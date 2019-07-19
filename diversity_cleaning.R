6024bc600d9e468bd79bfd5e5ef4318896dd447d
Steve Cauffman <sjcauffm@ncsu.edu>
2019-07-19 16:03:40 -0400

    Adjusted the entire script and now it gets the trimmed component names correctly.

--- diversity_cleaning.R
+++ diversity_cleaning.R
@@ -9,27 +9,18 @@ import <- gs_title("patternfly_adoption")
 data <- gs_read_csv(import)
 
 ##### need to isolate the component column
-component_names <- data$full_component  
-components_lower <- component_names %>% ### turns each element of the list to all lower case for easier cleaning
+component_names <- data[,c(2,4)]  
+component_names$lower <- component_names$full_component %>% ### turns each element of the list to all lower case for easier cleaning
   lapply(tolower)
 
-components_lower <- do.call(rbind.data.frame, components_lower)  ## unlists ouput to a data frame 
-
-components_lower$original <- data$full_component
-names(components_lower) <- c("component_names", "full_component") # changed the column name to something more readable
-
-
 ##### separating out the names with "import" in the name
-imports_grep <- grep("imports", components_lower$component_names)
+imports_grep <- grep("imports", component_names$lower)
 
-names_import <- components_lower[imports_grep,]
-names_import <- names_import %>%
-  as.data.frame()
+names_import <- component_names[imports_grep,]
 
-names(names_import) <- c("names", "full_component")
-names_import$names <- as.character(names_import$names)
+names_import$lower <- as.character(names_import$lower)
 
-imports_split <- strsplit(names_import$names, "react-")
+imports_split <- strsplit(names_import$lower, "react-")
 imports_split_names <- do.call(rbind.data.frame, imports_split)
 
 names(imports_split_names) <- c("extra", "component")
@@ -45,33 +36,27 @@ names(split_names5) <- c("component_name")
 names_import2 <- cbind(names_import, split_names5)
 
 ##### Getting the component names from components without "import" in the name.
-names_classes <- components_lower[-imports_grep,]
-names_classes <- names_classes %>%
-  as.data.frame()
+names_classes <- component_names[-imports_grep,]
 
-names(names_classes) <- c("names")
+names_classes$lower <- as.character(names_classes$lower)
 
-names_classes$names <- as.character(names_classes$names)
-
-names_split <- strsplit(names_classes$names, ".pf-")#### Need to rethink this one. 
+names_split <- strsplit(names_classes$lower, ".pf-")#### Need to rethink this one. 
 names_split2 <- lapply(names_split, tail, n = 1L)
 names_split3 <- do.call(rbind.data.frame, names_split2)
 
 names_classes <- cbind(names_classes, names_split3)
-names(names_classes) <- c("names", "full_component", "component_name")
+names(names_classes) <- c("full_component", "product", "lower", "component_name")
 
 ##### combining the names now and adding them to the main data frame. 
 names_combined <- rbind(names_import2, names_classes)
 names(names_combined) <- c("component_names", "full_component", "component")
 
-names_trim <- names_combined[,2:3]
+names_trim <- names_combined[,c(1:2,4)]
+names_trim$full_component <- as.character(names_trim$full_component)
 
 ##### Need to merge names_trim back with the main data frame in order get the real component names. 
 data2 <- data
-
-
-
-
+data3 <- full_join(data2, names_trim, by = c("full_component", "product"))
 
 
 
