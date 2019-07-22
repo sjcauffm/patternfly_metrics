304334e3f490b7b244af16c312696df1154967b5
Steve Cauffman <sjcauffm@ncsu.edu>
2019-07-22 14:50:05 -0400

    All fixed.

--- product_diversity_plotting.R
+++ product_diversity_plotting.R
@@ -20,21 +20,6 @@ comparison <- data3$component_name %in% pf_list$Component
 data3$is_component <- comparison
 
 pf_data <- data3[which(data3$is_component == TRUE),]
-
-#Need to re-clean some of the component name data. 
-unclean <- data3[which(data3$is_component == FALSE),]
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-
 save(pf_data, file = "patternfly_adoption_final.rda")
 
 ##### graphing product diversity
