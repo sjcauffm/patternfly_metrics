6af1bd43070ffc8423d4c81b80d6ceb6d8824b49
Steve Cauffman <sjcauffm@ncsu.edu>
2019-07-22 13:30:09 -0400

    Created the plot for Diversity

--- product_diversity_plotting.R
+++ product_diversity_plotting.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(ggplot2)
 library(googlesheets)
+library(ggthemes)
 
 ##### Plotting diversity of components for patternfly data. 
 load("/Volumes/GoogleDrive/My Drive/UXD-Share/Usability and User Research/Studies 2019/PatternFly Adoption Visualization/patternfly_metrics/patternfly_adoption_current.rda") #loads data file from shared drive
@@ -28,5 +29,13 @@ save(pf_data, file = "patternfly_adoption_final.rda")
 
 diversity <- table(pf_data$product)
 diversity_df <- as.data.frame(diversity)
-names(diversity_df) <- c("product", "componets")
+names(diversity_df) <- c("product", "components")
+
+div_plot <- ggplot(diversity_df, aes(x = reorder(product, -components), y = components)) +
+  geom_bar(stat = "identity") + theme_tufte() + 
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) + 
+  scale_x_discrete(expand = c(0, 0)) + scale_y_continuous(expand = c(0, 0), limits = c(0,40)) +
+  labs(x = "Product", y = "Number of PF Components", title = "Number of PF Components Used by Each Product")
+
+ggsave("diversity_plot.png", div_plot, height = 6, width = 10, units = "in")
 
