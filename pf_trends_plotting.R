744ea55ee575e3cc3550593eb84b58626eeb9ec1
Steve Cauffman <sjcauffm@ncsu.edu>
2019-07-30 16:59:57 -0400

    Need to decide if it's better to present the components trends data via bars or lines. Maybe want to facet instead.

--- pf_trends_plotting.R
+++ pf_trends_plotting.R
@@ -7,3 +7,13 @@ library(ggthemes)
 setwd("/Volumes/GoogleDrive/My Drive/UXD-Share/Usability and User Research/Studies 2019/PatternFly Adoption Visualization/patternfly_metrics") # if this isn't set. 
 
 load("/Volumes/GoogleDrive/My Drive/UXD-Share/Usability and User Research/Studies 2019/PatternFly Adoption Visualization/patternfly_metrics/patternfly_adoption_final.rda") ## loads the data into the file. 
+
+##### Graphing component imports over time. 
+components <- aggregate(pf_data$imports, by = list(pf_data$component, pf_data$date), FUN = sum)
+names(components) <- c("component", "date", "imports")
+
+components_trend <- ggplot(components, aes(x = date, y = imports, group = component, color = component)) +
+  geom_line(stat = "identity") + theme_tufte() +
+  theme(axis.text.x = element_text(angle = 60, vjust = 1, hjust = 1),
+        text = element_text(family = "Red Hat Display")) + 
+  scale_x_discrete(expand = c(0, 0)) + scale_y_continuous(expand = c(0, 0), limits = c(0, 275))
