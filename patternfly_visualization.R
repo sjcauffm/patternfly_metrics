d8d68db33ecc3e71cfa6c22dc086570d3f4e006d
Steve Cauffman <sjcauffm@ncsu.edu>
2019-08-02 10:33:33 -0400

    altered the code to relfect more up to date data.

--- patternfly_visualization.R
+++ patternfly_visualization.R
@@ -9,7 +9,8 @@ setwd("/Volumes/GoogleDrive/My Drive/UXD-Share/Usability and User Research/Studi
 load("/Volumes/GoogleDrive/My Drive/UXD-Share/Usability and User Research/Studies 2019/PatternFly Adoption Visualization/patternfly_metrics/patternfly_adoption_final.rda")
 
 components_sum <- aggregate.data.frame(pf_data$imports, by = list(pf_data$component, pf_data$date), FUN = sum)
-components_mean <- aggregate.data.frame(pf_data$imports, by = list(pf_data$component), FUN = mean) ## counts the sum of imports for each component
+components_totals <- aggregate.data.frame(pf_data$imports, by = list(pf_data$component), FUN = sum) ## counts the sum of imports for each component
+components_mean <- aggregate.data.frame(pf_data$imports, by = list(pf_data$component), FUN = mean)
 products <- aggregate.data.frame(pf_data$imports, by = list(pf_data$product), FUN = sum) ## counts the sum of imports for each product, gives total imports for each product
 versions <- aggregate.data.frame(pf_data$imports, by = list(pf_data$version), FUN = sum) ## counts the number of imports by patternfly version
 products_versions <- aggregate(pf_data$imports, by = list(pf_data$product, pf_data$version), FUN = sum) ## counts the number of imports by each product for each patternfly version
@@ -44,32 +45,32 @@ products_plot <- ggplot(products, aes(x = product, y = imports)) +
 ggsave("Product_imports_plot.png", products_plot, width = 10, height = 6, units = "in")
 
 ##### Graphing top components ####
-names(components_sum) <- c("component", "imports_sum")
+names(components_totals) <- c("component", "imports_sum")
 names(components_mean) <- c("component", "imports_mean")
 
-components_sum$imports_mean <- components_mean$imports_mean 
-components_sum$imports_prop <- components_sum$imports_sum/sum(components_sum$imports_sum)
+components_totals$imports_mean <- components_mean$imports_mean 
+components_totals$imports_prop <- components_totals$imports_sum/sum(components_totals$imports_sum)
 
-components_sum$adj_prop <- (components_sum$imports_sum + 2) / (sum(components_sum$imports_sum) + 4) ###get adjusted sample proportion
-components_sum$x <- components_sum$adj_prop * (1 - components_sum$adj_prop) ###standard error calculation - step 1
-components_sum$y <- components_sum$x/(sum(components_sum$imports_sum) + 4) ###standard error calculation - step 2
-components_sum$z <- sqrt(components_sum$y) ###standard error calculation - step 3
-components_sum$margin_error <- components_sum$z * 2 ###gets margin of error
-components_sum$upper_ci <- components_sum$imports_prop + components_sum$margin_error ###calculate upper ci
-components_sum$lower_ci <- components_sum$imports_prop - components_sum$margin_error
+components_totals$adj_prop <- (components_totals$imports_sum + 2) / (sum(components_totals$imports_sum) + 4) ###get adjusted sample proportion
+components_totals$x <- components_totals$adj_prop * (1 - components_totals$adj_prop) ###standard error calculation - step 1
+components_totals$y <- components_totals$x/(sum(components_totals$imports_sum) + 4) ###standard error calculation - step 2
+components_totals$z <- sqrt(components_totals$y) ###standard error calculation - step 3
+components_totals$margin_error <- components_totals$z * 2 ###gets margin of error
+components_totals$upper_ci <- components_totals$imports_prop + components_totals$margin_error ###calculate upper ci
+components_totals$lower_ci <- components_totals$imports_prop - components_totals$margin_error
 
-components_trim <- components_sum[which(components_sum$imports_prop < .001) ,]
+components_trim <- components_sum[which(components_totals$imports_prop < .001) ,]
 
-components_plot <- ggplot(components_sum, aes(x = reorder(component, -imports_prop), y = imports_prop)) + 
-  geom_bar(stat = "identity", position= position_dodge()) + 
-  geom_errorbar(ymin = components_sum$imports_prop - components_sum$margin_error, 
-                                              ymax = components_sum$imports_prop + components_sum$margin_error, 
+components_plot <- ggplot(components_totals, aes(x = reorder(component, -imports_prop), y = imports_prop)) + 
+  geom_bar(stat = "identity") + 
+  geom_errorbar(ymin = components_totals$imports_prop - components_totals$margin_error, 
+                                              ymax = components_totals$imports_prop + components_totals$margin_error, 
                                               width = .5) +
   theme_tufte() +
   theme(axis.text.x = element_text(angle = 75, vjust = 1, hjust = 1),
         text = element_text(family = "Red Hat Display")) + 
-  scale_x_discrete(expand = c(0, 0), labels = components_sum$component) + scale_y_continuous(expand = c(0, 0), limits = c(0, .185)) +
-  labs(x = "Component", y = "Proportion of Total Imports", title = "Top Components as a Proportion of Total Component Imports")
+  scale_x_discrete(expand = c(0, 0)) + scale_y_continuous(expand = c(0, 0), limits = c(0, .2)) +
+  labs(x = "Component", y = "Proportion of Total Imports", title = "Top Components as a Proportion of Total Component Imports as of 07-2019")
   
 ggsave("Top_Components.png", components_plot, width = 24, height = 16, units = "in")
 
@@ -89,6 +90,8 @@ component_totals <- ggplot(components_sum, aes(x = component, y = imports, fill 
   theme(axis.text.x = element_text(angle = 60, vjust = 1, hjust = 1),
         text = element_text(family = "Red Hat Display")) +
   labs(x = "Component", y = "Total Number of Imports", title = "Number of Component Imports per Month", fill = "Date")
+
+ggsave(filename = "component_totals.png", component_totals, height = 12, width = 20, units = "in")
 
 
 ##### Splitting the products by portfolio membership. #####
